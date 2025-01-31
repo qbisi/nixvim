37fe8be790b3f8dcd904bfb59260c1be3ecafe5a
qbisi <qbisicwate@gmail.com>
2025-02-01 03:40:18 +0800

    fix

--- config/plugins/neo-tree.nix
+++ config/plugins/neo-tree.nix
@@ -1,16 +1,35 @@
 {
   plugins.neo-tree = {
     enable = true;
-    # filesystem.hijackNetrwBehavior = "disabled";
+    window.mappings."<left>".__raw = ''
+      function(state)
+        local node = state.tree:get_node()
+        if node.type == "directory" and node:is_expanded() then
+          require("neo-tree.sources.filesystem").toggle_directory(state, node)
+        else
+          require("neo-tree.ui.renderer").focus_node(state, node:get_parent_id())
+        end
+      end
+    '';
+    window.mappings."<right>".__raw = ''
+      function(state)
+        local node = state.tree:get_node()
+        if node.type == "directory" then
+          if not node:is_expanded() then
+            require("neo-tree.sources.filesystem").toggle_directory(state, node)
+          elseif node:has_children() then
+            require("neo-tree.ui.renderer").focus_node(state, node:get_child_ids()[1])
+          end
+        else
+          require("neo-tree.sources.common.preview").show(state)
+        end
+      end
+    '';
   };
-  keymaps = [
-    {
-      mode = [ "n" ];
-      key = "<leader>e";
-      action = "<cmd>Neotree toggle<CR>";
-      options = {
-        desc = "Toggle Neo-Tree";
-      };
-    }
-  ];
+  keymaps = [{
+    mode = [ "n" ];
+    key = "<leader>e";
+    action = "<cmd>Neotree toggle<CR>";
+    options = { desc = "Toggle Neo-Tree"; };
+  }];
 }
